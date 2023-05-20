203da4fd4f0c07dabed8121f5015317a57947c2e
ManavKumar <manavpate0@gmail.com>
2023-05-20 15:36:02 +0300

    Initial

---
+++ index.js
@@ -0,0 +1,15 @@
+// console.log('Hello World')
+
+// for(let i=0; i<100;i++){
+//     console.log('hi')
+// }
+
+// const myPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('Success')
+//     },1000)
+// })
+// myPromise.then(data => {
+//     console.log(data)
+// })
+
