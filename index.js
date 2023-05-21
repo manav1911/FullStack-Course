f75dc675ad5540fb4f40c1215c11f5fff9bb4e32
ManavKumar <manavpate0@gmail.com>
2023-05-21 13:48:42 +0300

    NodeJS

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
-// // console.log(require)
-// // console.log(require('./math'))
+// // // console.log(require)
+// // // console.log(require('./math'))
 
-// const math = require('./math')
-// // console.log(math.add(1,2,22,45,23,45,67,89,90,100))
-// console.log(math)
+// // const math = require('./math')
+// // // console.log(math.add(1,2,22,45,23,45,67,89,90,100))
+// // console.log(math)
 
-const fs = require('fs')
+// const fs = require('fs')
 
-//WwriteFile over writes a file
-fs.writeFile('./newFile.txt','this is file is overwritten', (error) => {
-    if(error) {
-        console.log(error)
-        return
-    }
-    else{
-        console.log('File Created')
-    }
-})
-
-
-// //Asynchronous function, takes the path of your file
-// //When using readFile make sure to mention the encoding type
-// fs.readFile('./temp.txt','utf-8', (error,data) => {
-//     if(error){
+// //writeFile over writes a file
+// fs.writeFile('./newFile.txt','this is file is overwritten', (error) => {
+//     if(error) {
 //         console.log(error)
 //         return
 //     }
 //     else{
-//         console.log(data)
-//     }  
+//         console.log('File Created')
+//     }
 // })
 
-// const data = fs.readFileSync('./temp.txt')
-// console.log(data.toString())
+
+// // //Asynchronous function, takes the path of your file
+// // //When using readFile make sure to mention the encoding type
+// // fs.readFile('./temp.txt','utf-8', (error,data) => {
+// //     if(error){
+// //         console.log(error)
+// //         return
+// //     }
+// //     else{
+// //         console.log(data)
+// //     }  
+// // })
+
+// // const data = fs.readFileSync('./temp.txt')
+// // console.log(data.toString())
+
+/*
+    LOCAL MODULES
+*/
+
+const aws = require('aws-sdk')
+console.log(aws)
