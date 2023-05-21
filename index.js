a7f588ff6805107c32bf8b894ee033a4fff785b3
ManavKumar <manavpate0@gmail.com>
2023-05-21 13:33:00 +0300

    node

---
+++ index.js
@@ -0,0 +1,33 @@
+// // console.log(require)
+// // console.log(require('./math'))
+
+// const math = require('./math')
+// // console.log(math.add(1,2,22,45,23,45,67,89,90,100))
+// console.log(math)
+
+const fs = require('fs')
+
+//WwriteFile over writes a file
+fs.writeFile('./newFile.txt','this is file is overwritten', (error) => {
+    if(error) {
+        console.log(error)
+        return
+    }
+    else{
+        console.log('File Created')
+    }
+})
+
+// //Asynchronous function, takes the path of your file
+// fs.readFile('./temp.txt','utf-8', (error,data) => {
+//     if(error){
+//         console.log(error)
+//         return
+//     }
+//     else{
+//         console.log(data)
+//     }  
+// })
+
+// const data = fs.readFileSync('./temp.txt')
+// console.log(data.toString())
--- math.js
+++ math.js
@@ -1,2 +1,14 @@
-module.exports.add = (...rest)=> rest.reduce((acc, val)=> acc + val, 0)
-module.exports.multiply = (...rest)=> rest.reduce((acc, val)=> acc * val, 1)
+const add = (...rest)=> rest.reduce((acc, val)=> acc + val, 0)
+const multiply = (...rest)=> rest.reduce((acc, val)=> acc * val, 1)
+
+// module.exports = {
+//     add,
+//     multiply,
+//     something: 'else',
+//     age:22
+// }
+
+console.log('This is a log from the math module')
+
+// module.exports = add
+module.exports = [1,2,5]
