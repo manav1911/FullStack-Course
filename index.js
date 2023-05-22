c92d1c1dda979ad7c505c525be0efc5c54b9cd25
ManavKumar Patel <manavpate0@gmail.com>
2023-05-22 11:58:26 +0300

    Day-34

---
+++ index.js
@@ -0,0 +1,25 @@
+// const {v4:uuidv4} = require('uuid')
+// const fetch = require('node-fetch')
+
+// // console.log(fetch)
+
+// const id = uuidv4()
+// console.log(id)
+
+// const fetchData = async() => {
+//     try{
+//         const res = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+//         const data = await res.json()
+//         console.log(data)
+//     }
+//     catch(error){
+//         console.log(error)
+//     }
+// }
+// fetchData()
+
+console.log('h1')
+
+for(let i=0; i<100;i++){
+    console.log(i)
+}
