c80c23462273059037360adc2ba0095a071c2085
ManavKumar Patel <manavpate0@gmail.com>
2023-05-23 10:09:30 +0300

    Params

--- index.js
+++ index.js
@@ -4,31 +4,45 @@ const app = express()
 const fs = require('fs')
 
 //The get method below does the following: So when we make a request to the slash route the 
-app.get('/', (request,response) => {
+app.get('/', (request, response) => {
     response.send('First response from express server!!').status(200)
 })
 
+//Params
+app.get('/name/:username', (req,res) => {
+    console.log(req.params)
+    const {username} = req.params
+    res.status(200).send(`My name is ${username}`)
+})
+// app.get('/product/:name', (req,res) => {
+    // console.log(req.params.name)
+    // const {name} = req.params
+    // res.status(200).send(`My name is ${username}`)
+// })
 
-app.get('/todos',(req,res)=>{
-    console.log({
-        headers:req.headers,
-        body:req.body,
-        params:req.params,
-        query:req.query
-    })
-    fs.readFile('./db.js','utf-8',(err,data)=>{
-        if(err){
-            console.log(err)
-        }
-        // //This is a textual response
-        // res.status(200).send(
-        //     data
-        // )
-        //Send as an object converts to JSON response
-        res.status(200).json(
-            JSON.parse(data)
-        )
-    })
+app.get("/todos", (req, res) => {
+    // console.log({
+    //     headers:req.headers,
+    //     body:req.body,
+    //     params:req.params,
+    //     query:req.query
+    // })
+    const { count } = req.query
+    if (count) {
+        fs.readFile("./db.js", "utf-8", (err, data) => {
+            if (err) {
+                console.log(err)
+            }
+            res.status(200).json(JSON.parse(data).slice(0, count))
+        })
+    } else {
+        fs.readFile("./db.js", "utf-8", (err, data) => {
+            if (err) {
+                console.log(err)
+            }
+            res.status(200).json(JSON.parse(data));
+        })
+    }
 })
 
 app.listen(port, () => {
