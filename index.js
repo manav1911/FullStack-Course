941481eb25b68f3f52aff50341dfa6308c974ee8
ManavKumar Patel <manavpate0@gmail.com>
2023-05-23 09:13:34 +0300

    JSON Place Holder

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const port = 8080
 const app = express()
+const fs = require('fs')
 
 //The get method below does the following: So when we make a request to the slash route the 
 app.get('/', (request,response) => {
-    console.log(request.headers)
-    console.log(request.path)
-    console.log('Hello World')
     response.send('First response from express server!!').status(200)
+})
+
+
+app.get('/todos',(req,res)=>{
+    fs.readFile('./db.js','utf-8',(err,data)=>{
+        if(err){
+            console.log(err)
+        }
+        // //This is a textual response
+        // res.status(200).send(
+        //     data
+        // )
+        //Send as an object converts to JSON response
+        res.status(200).json(
+            JSON.parse(data)
+        )
+        
+    })
 })
 
 app.listen(port, () => {
