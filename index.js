ae3bac4366fbfed76aded5ff13cb3a9f1e5107e2
ManavKumar Patel <manavpate0@gmail.com>
2023-05-23 08:10:15 +0300

    FirstExpress

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-// const {v4:uuidv4} = require('uuid')
-// const fetch = require('node-fetch')
+const express = require('express')
+const port = 8080
+const app = express()
 
-// // console.log(fetch)
-
-// const id = uuidv4()
-// console.log(id)
-
-// const fetchData = async() => {
-//     try{
-//         const res = await fetch('https://jsonplaceholder.typicode.com/posts/1')
-//         const data = await res.json()
-//         console.log(data)
-//     }
-//     catch(error){
-//         console.log(error)
-//     }
-// }
-// fetchData()
-
-console.log('h1')
-
-for(let i=0; i<100;i++){
-    console.log(i)
-}
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
