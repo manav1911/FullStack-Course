d7793f616490737d1220e6407191ecd23655a7a4
ManavKumar Patel <manavpate0@gmail.com>
2023-05-23 08:46:05 +0300

    Request&Response

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const port = 8080
 const app = express()
 
+//The get method below does the following: So when we make a request to the slash route the 
+app.get('/', (request,response) => {
+    console.log(request.headers)
+    console.log(request.path)
+    console.log('Hello World')
+    response.send('First response from express server!!').status(200)
+})
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
